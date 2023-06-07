0550d44b8726dab2052bb51d0f91780865339235
Moritz Schmitz von Hülst <moritz.schmitz@oviva.com>
2023-06-07 21:21:58 +0200

    NA: Init

---
+++ .github/scripts/notify-commit-author.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const commentSignature = '<!-- notify-commit-author.js -->';
+
+module.exports = async ({github, context, commits}) => {
+    // Create message
+    const messageHeader = `${commentSignature}\n${context.actor}, please consider cherry-picking these commits into 'origin/main':\n`;
+    const message = commits
+        .split(' ')
+        .reduce((str, commit) => `${str}${commit}\n`, messageHeader);
+
+    // Get the latest comment, if any
+    const {data: comments} = await github.rest.issues.listComments({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: context.issue.number,
+    });
+    const comment = comments.find(c => c.body.includes(commentSignature));
+
+    // Delete comment if it exists
+    if (comment) {
+        await github.rest.issues.deleteComment({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            comment_id: comment.id,
+        });
+    }
+
+    // Create comment if there is something to comment on
+    if (message !== '') {
+        await github.rest.issues.createComment({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: context.issue.number,
+            body: message,
+        });
+    }
+}
