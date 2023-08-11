0cfc569130d7493fffeef81d22b3541b922c2da8
Moritz Schmitz von Hülst <moritz.schmitz@oviva.com>
2023-08-11 09:56:21 +0200

    Can I enable branch protection from workflows?

--- .github/scripts/enable-branch-protection.js
+++ .github/scripts/enable-branch-protection.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
 module.exports = async ({github, context}) => {
+    await github.rest.repos.updateBranchProtection({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        branch: 'main',
+    })
     console.log(github, context);
 }
